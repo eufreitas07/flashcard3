f82f4383381d0be0f64c631668e6523110e2154c
isaias freitas <isaiasfreitas2007@gmail.com>
2024-11-02 13:48:57 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Programação',
+    'O que é JavaScript?',
+    'O JavaScript é uma linguagem de Programção'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Língua inglesa',
+    'Como se diz oi em inglês?',
+    'Oi em inglês é HI'
+)
+
+criaCartao(
+    'Futebol',
+    'Em que ano foi fundado o Corinthians?',
+    'No ano de 1910'
+)
+
+criaCartao(
+    'Futebol',
+    'Contra qual time o Corinthians foi campeão mundial em 2012?',
+    'Foi contra o Chelsea'
+)
